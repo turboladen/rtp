be3a465dca341c57323af8f0cde9054302de56fc
Steve Loveless <steve.loveless@gmail.com>
2012-11-03 12:08:26 -0700

    Parse extension header

--- lib/rtp/packet.rb
+++ lib/rtp/packet.rb
@@ -4,6 +4,7 @@ module RTP
   class Packet < BinData::Record
     endian :big
 
+    # RTP Header
     bit2 :version
     bit1 :padding
     bit1 :extension
@@ -18,8 +19,8 @@ module RTP
     array :csrc_ids, :type => :uint32, :initial_length => lambda { csrc_count }
 
     # Extension header is variable length if :extension == 1
-    #uint16 :extension_id
-    #uint16 :extension_length
+    uint16 :extension_id, onlyif: :has_extension?
+    uint16 :extension_length, onlyif: :has_extension?
 
 =begin
   # h.264 payload
@@ -38,5 +39,9 @@ module RTP
 =end
     count_bytes_remaining :bytes_remaining
     string :rtp_payload, read_length: lambda { bytes_remaining }
+
+    def has_extension?
+      extension == 1
+    end
   end
 end
