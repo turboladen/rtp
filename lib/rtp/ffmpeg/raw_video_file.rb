770003a802d82652a8e82e3b373d5837de83958b
Steve Loveless <steve.loveless@gmail.com>
2013-01-01 23:27:43 -0800

    Added docs

--- lib/rtp/ffmpeg/raw_video_file.rb
+++ lib/rtp/ffmpeg/raw_video_file.rb
@@ -4,7 +4,14 @@ require_relative 'api/av_picture'
 
 module RTP
   module FFmpeg
+
+    # Handles writing raw video frames to a file.
     class RawVideoFile
+
+      # @param [String] file_name The name/path of the file to write to.
+      # @param [Fixnum] width Width of the frames to write.
+      # @param [Fixnum] height Height of the frames to write.
+      # @param [Fixnum] pixel_format Pixel format of the frames to write.
       def initialize(file_name, width, height, pixel_format)
         @file = RTP::LibC.fopen(file_name, 'wb')
         @width = width
@@ -37,12 +44,19 @@ module RTP
         RTP::FFmpeg.av_freep(dest_picture)
       end
 
+      # Closes the file.
+      #
+      # @return [Fixnum] 0 if it was successfully closed.
       def close
         RTP::LibC.fclose(@file)
       end
 
       private
 
+      # Creates the destination AVPicture structure that will be used to copy
+      # the originating frame from.  This is necessary for writing raw data
+      # since a raw data file must have pictures all aligned.
+      #
       # @return [Array<RTP::FFmpeg::AVPicture,Fixnum>]
       def init_destination_picture
         dest_picture = RTP::FFmpeg.avcodec_alloc_frame
