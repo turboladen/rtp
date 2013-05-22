bbff6ab243fad30ef32a0f347320de3fc37532cb
Steve Loveless <steve.loveless@gmail.com>
2013-05-22 10:59:52 -0700

    Get initial packet separately so subsequent packets are in an array of proper size

--- lib/rtp/rtcp_packet.rb
+++ lib/rtp/rtcp_packet.rb
@@ -1,18 +1,49 @@
 require 'bindata'
-require_relative 'rtcp_packets/sender_report'
+Dir[File.dirname(__FILE__) + '/rtcp_packets/*.rb'].each(&method(:require))
 
 
 module RTP
   class RTCPPacket < BinData::Record
-    PACKET_TYPES = {
-      200 => RTP::RTCPPackets::SenderReport
+    FIRST_PACKET_TYPES = {
+      200 => RTP::RTCPPackets::SenderReport,
+      201 => RTP::RTCPPackets::ReceiverReport
+    }
+
+    COMPOUND_PACKET_TYPES = {
+      202 => RTP::RTCPPackets::SourceDescription
     }
 
     endian :big
 
-    array :packets, initial_length: 6 do
+    #---------------------------------------------------------------------------
+    # RTCP Header
+    #---------------------------------------------------------------------------
+
+    # @return [BinData::Bit2]
+    bit2 :version
+
+    # @return [BinData::Bit1]
+    bit1 :padding
+
+    # @return [BinData::Bit5]
+    bit5 :item_count
+
+    # @return [BinData::Bit8]
+    bit8 :packet_type
+
+    # @return [BinData::Uint32be]
+    uint16 :packet_length
+
+    # @return [BinData::Choice]
+    choice :packet_data, choices: FIRST_PACKET_TYPES, selection: :packet_type
+
+    # @return [BinData::String]
+    string :pad_data, read_length: lambda { self.padding.zero? ? 0 : 8 }
+
+    # @return [BinData::Array]
+    array :packets, read_until: lambda { self.item_count } do
       #---------------------------------------------------------------------------
-      # RTCP Header
+      # Header
       #---------------------------------------------------------------------------
 
       # @return [BinData::Bit2]
@@ -31,7 +62,7 @@ module RTP
       uint16 :packet_length
 
       # @return [BinData::Choice]
-      choice :packet_data, choices: PACKET_TYPES, selection: :packet_type
+      choice :packet_data, choices: COMPOUND_PACKET_TYPES, selection: :packet_type
 
       # @return [BinData::String]
       string :pad_data, read_length: lambda { self.padding.zero? ? 0 : 8 }
